5a8ca0be7354568951da98780f08418b8ba263ec
Dw37 <kpdwyer@live.com>
2017-11-09 01:20:45 -0500

    Road Nodes (Traffic pathfinding)

---
+++ CarGame/Car Game/Assets/Scripts/Editor/RoadNodeInspector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoadNode))]
+public class RoadNodeInspector : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUI.changed)
+        {
+            ((RoadNode)target).ValidateData();
+        }
+    }
+}
---
+++ CarGame/Car Game/Assets/Scripts/RoadNode.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadNode : MonoBehaviour
+{
+
+    public List<RoadNode> m_Neighbours;
+
+    // Use this for initialization
+    void Start()
+    {
+		
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+
+        if (m_Neighbours != null)
+        {
+            foreach (RoadNode a in m_Neighbours)
+            {
+                Gizmos.DrawLine(transform.position, a.transform.position);
+            }
+        }
+    }
+
+    public void ValidateData()
+    {
+
+        //we ensure the relationship is 2 ways
+        foreach (RoadNode a in m_Neighbours)
+        {
+            if (!a.m_Neighbours.Contains(this))
+            {
+                a.m_Neighbours.Add(this);
+            }
+        }
+    }
+}
